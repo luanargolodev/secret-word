e4db56ea85410f1080a077e5519e3fe9126b3bdb
Luan Argolo <ipsluan@icloud.com>
2022-05-29 01:59:11 -0300

    feat: Defining the game stages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import "./App.css";
 
 import StartScreen from "./components/StartScreen";
 
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 const App = () => {
   return (
     <div className="App">
