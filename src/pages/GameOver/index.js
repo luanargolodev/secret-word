9599f653e7c286fcb34c2ba53000651819cb6d75
Luan Argolo <ipsluan@icloud.com>
2022-05-29 13:41:27 -0300

    refactor: Add styled-components on the GameOver page

--- src/pages/GameOver/index.js
+++ src/pages/GameOver/index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import { Score } from "./styles";
 
 import { Button } from "../../components/Button";
 import { Title } from "../../components/Title";
@@ -8,7 +8,7 @@ const GameOver = ({ retry, score }) => {
     <div>
       <Title>Fim de jogo!</Title>
       <h2>
-        A sua pontuação foi: <span>{score}</span>
+        A sua pontuação foi: <Score>{score}</Score>
       </h2>
       <Button onClick={retry}>Reiniciar jogo</Button>
     </div>
---
+++ src/pages/GameOver/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Score = styled.span`
+  color: #ecfa00;
+  font-size: 1.5em;
+`;
