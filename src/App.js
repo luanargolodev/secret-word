43d7e63b585ee0d3f994be366e9da9916b8e5cf5
Luan Argolo <ipsluan@icloud.com>
2022-05-29 02:35:51 -0300

    feat: Set all letters to lowercase

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ const App = () => {
   const startGame = () => {
     const { category, word } = pickWordAndCategory();
     let wordLetters = word.split("");
+    wordLetters = wordLetters.map((letter) => letter.toLowerCase());
 
     setGameStage(stages[1].name);
   };
