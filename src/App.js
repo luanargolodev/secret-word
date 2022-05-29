6b06a6b85146646a6fa589510ffeb36b61576dbd
Luan Argolo <ipsluan@icloud.com>
2022-05-29 03:26:17 -0300

    feat: Functions in the fields to enter the letter

--- src/App.js
+++ src/App.js
@@ -45,8 +45,21 @@ const App = () => {
     setGameStage(stages[1].name);
   };
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    const normalizedLetter = letter.toLowerCase();
+
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    )
+      return;
+
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters([...guessedLetters, normalizedLetter]);
+    } else {
+      setWrongLetters([...wrongLetters, normalizedLetter]);
+      setGuesses(guesses - 1);
+    }
   };
 
   const retry = () => {
