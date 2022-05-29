99be2249ea44f20889ca6cb1f2c61748d173c547
Luan Argolo <ipsluan@icloud.com>
2022-05-29 01:56:25 -0300

    feat: Style button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
 import "./App.css";
+
+import StartScreen from "./components/StartScreen";
 
 const App = () => {
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+      <StartScreen />
     </div>
   );
 };
